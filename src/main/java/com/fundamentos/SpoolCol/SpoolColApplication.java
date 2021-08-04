e624e0f183ffa7d10d18d6bb47cdf2a2f5449612
Jesus Perez <japerezb.col@gmail.com>
2021-08-04 14:40:47 -0500

    first commit

---
+++ src/main/java/com/fundamentos/SpoolCol/SpoolColApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos.SpoolCol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoolColApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoolColApplication.class, args);
+	}
+
+}
